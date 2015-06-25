f80d744eb91a10794c1af18554f62b31ea24d861
Kristjan Rang <mail@rang.ee>
2015-06-25 12:12:44 +0300

    Basically working

---
+++ screenshot.js
@@ -0,0 +1,15 @@
+var system = require('system'),
+    page = require('webpage').create();
+
+page.viewportSize = { width: 1000, height: 1000 };
+page.open(system.args[1], function (status) {
+  if (status !== 'success') {
+    console.log('Unable to access the network!');
+    phantom.exit();
+  } else {
+    setTimeout(function() {
+      page.render(system.args[2]);
+      phantom.exit();
+    }, system.args[3]); // wait for sign to render and fonts/images to load and settle
+  }
+});
--- web.go
+++ web.go
@@ -1,25 +1,68 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
-  "os/exec"
-  "os"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strconv"
+)
+
+var phantomPath string
+var scriptPath string
+
+const (
+	VERSION 		= "0.0.1"
+	VERSIONFANCY 	= "Spooky Phantom"
 )
 
 func main() {
-  http.HandleFunc("/", test)
-  fmt.Println("listening...")
-  err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
-  if err != nil {
-    panic(err)
-  }
+	path, pathErr := exec.LookPath("phantomjs")
+	if pathErr != nil {
+		log.Fatal("installing phantomjs is in your future")
+	}
+	phantomPath = path
+    scriptPath, _ = filepath.Abs(os.Args[1])
+
+	http.HandleFunc("/screenshot", screenshot)
+	http.HandleFunc("/version", version)
+	fmt.Println("listening...")
+	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	if err != nil {
+		panic(err)
+	}
 }
 
-func test(res http.ResponseWriter, req *http.Request) {
-  path, _ := exec.LookPath("phantomjs")
-  fmt.Fprintln(res, path)
-  
-  out, _ := exec.Command(path, "-v").Output()
-  fmt.Fprintln(res, out)
+func version(res http.ResponseWriter, req *http.Request) {
+	out, _ := exec.Command(phantomPath, "-v").Output()
+	fmt.Fprintf(res, "Version %s %s\n", VERSION, VERSIONFANCY)
+	fmt.Fprintf(res, "Phantom version is %s\n", out)
+}
+
+func screenshot(res http.ResponseWriter, req *http.Request) {
+	req.ParseForm()
+	url := req.Form.Get("url")
+	waitStr := req.Form.Get("wait")
+	if url == "" || waitStr == "" {
+		fmt.Fprintln(res, "must specify URL and wait time")
+		return
+	}
+
+	wait, _ := strconv.Atoi(waitStr)
+	file, _ := ioutil.TempFile(os.Getenv("TMPDIR"), "screenshot")
+	tmpPath := file.Name() + ".png"
+	file.Close()
+	os.Remove(file.Name())
+
+	out, cmdErr := exec.Command(phantomPath, scriptPath, url, tmpPath, strconv.Itoa(wait * 1000)).CombinedOutput()
+	if cmdErr != nil {
+		fmt.Fprintf(res, "Error screenshotting page: %s\n", out)
+		return
+	}
+	defer os.Remove(tmpPath)
+
+	http.ServeFile(res, req, tmpPath)
 }
